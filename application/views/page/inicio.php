80ca6d1518b2355feff9d41b9572e64394f55fa9
JDarkGreen <jgomez.4net@gmail.com>
2016-05-13 18:49:11 -0500

    update estilos

--- application/views/page/inicio.php
+++ application/views/page/inicio.php
@@ -75,9 +75,15 @@
 				<!-- Banner1 -->
 				<div class="col-xs-12 col-md-4">
 					<section id='slider-home-1' class="nivoSlider sectionPage__banner">
-						<img src="<?= IMAGES ?>/banners/banner1_4.jpg" alt="" class="img-responsive" />
-						<img src="<?= IMAGES ?>/banners/banner1_5.jpg" alt="" class="img-responsive" />
-						<img src="<?= IMAGES ?>/banners/diseno-paginas-web-centrolima-peru.jpg" alt="diseno-paginas-web-centrolima-peru" class="img-responsive" />
+						<a href="javascript:void(0)">
+							<img src="<?= IMAGES ?>/banners/banner1_4.jpg" alt="" class="img-responsive" />
+						</a>
+						<a href="https://www.facebook.com/koalapublicidad.pe?ref=tn_tnmn" target="_blank">
+							<img src="<?= IMAGES ?>/banners/banner1_5.jpg" alt="" class="img-responsive" />
+						</a>
+						<a href="javascript:void(0)">
+							<img src="<?= IMAGES ?>/banners/diseno-paginas-web-centrolima-peru.jpg" alt="diseno-paginas-web-centrolima-peru" class="img-responsive" />
+						</a>
 					</section> <!-- /.sectionPage__banner1 -->
 				</div><!-- /.col-xs-4 -->			
 
